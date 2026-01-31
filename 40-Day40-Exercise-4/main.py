15196d17a9cc2c664de609ec99442d53fefa2a41
Manojrampal <68961033+Manojrampal@users.noreply.github.com>
2026-01-31 23:04:29 +0530

    Adding code for exercise 4 main.py

--- 40-Day40-Exercise-4/main.py
+++ 40-Day40-Exercise-4/main.py
@@ -12,3 +12,58 @@
 #   remove 3 random characters from start and end. Now remove the last letter and append it to the beginning
 
 # Your program should ask whether you want to code or decode
+
+
+import random
+import string
+
+
+def code(x):
+	try:
+		if( len(x) < 3):
+			value=x[::-1]
+			return value
+		else:
+			val=x[1:len(x)]+x[0]
+
+			first3=random.choices(string.ascii_letters,k=3)
+			last3=random.choices(string.ascii_letters,k=3)
+			first=''
+			last=''
+			for i in first3:
+				first= first+i
+			for i in last3:
+				last=last+i
+			
+			final=first+val+last
+			return final
+	except:
+		print("Error occurred while coding")
+		return 0
+    
+    
+def decode(x):
+    try:
+    	if( len(x) < 3):
+    		value=x[::-1]
+    		return value
+    	else:
+    		val=x[-4]+x[3:len(x)-4]
+    		return val
+    	
+    except:
+    	print("Some error occurred while decoding")
+    	return 0
+
+choice = input("Do you want to code or decode ? ")
+if choice.upper() == "CODE":
+	val = input("Enter a word to code ")
+	x = code(val)
+	print(x)
+	
+elif choice.upper() == "DECODE":
+	val = input("Enter a word to decode ")
+	x=decode(val)
+	print(x)
+else:
+	print("Please enter correct value : (Code/Decode) ")
